7a2e93eea6076d67d93eb5db3f90c8e7f064ef59
Barrett Witkin <bwitkin@gmail.com>
2013-08-22 11:38:32 -0400

    Modified db config files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bloccit
+
+A Reddit-style application to demonstrate how much I have to learn, to inspire/infuriate me in the process of its creation, and to establish foundation programming and web development skills.
+
+Made with my mentor at [Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = '0453c84b35ccd59f12b85b115f02dba8ff88803881cddb8ee6b8c580dfc77e30192163ad09be311645ad796eb5f2777d86107710e6cec7b85d6b9bdbb393b7da'
